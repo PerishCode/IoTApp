0e0fc0aa32a03e45b8a0fbed7a60fc4196c5ac8d
PerishCode <perishcode@gmail.com>
2020-10-24 09:41:26 +0800

    LAN test success

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,12 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '做一个长寿的程序员',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    // motto: '做一个长寿的程序员 yeah',
+    // userInfo: {},
+    // hasUserInfo: false,
+    // canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    devices: [{ id: 'a', name: '饮水机' }],
+    success: '未发起请求',
   },
   //事件处理函数
   bindViewTap: function () {
@@ -16,39 +18,63 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = (res) => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true,
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: (res) => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true,
-          })
-        },
-      })
-    }
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true,
+      success: '已发起请求',
     })
+
+    wx.request({
+      url: 'http://192.168.31.29:7777/api/devices',
+      method: 'GET',
+      success: (res) => {
+        // this.setData({
+        //   devices: res.data,
+        // })
+
+        this.setData({
+          success: '请求成功',
+          devices: res.data,
+        })
+      },
+      fail: (res) => {
+        // console.log(res);
+        this.setData({
+          success: '请求失败' + res.errMsg,
+        })
+      },
+    })
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true,
+    //   })
+    // } else if (this.data.canIUse) {
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = (res) => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true,
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: (res) => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true,
+    //       })
+    //     },
+    //   })
+    // }
   },
+  // getUserInfo: function (e) {
+  //   console.log(e)
+  //   app.globalData.userInfo = e.detail.userInfo
+  //   this.setData({
+  //     userInfo: e.detail.userInfo,
+  //     hasUserInfo: true,
+  //   })
+  // },
 })
